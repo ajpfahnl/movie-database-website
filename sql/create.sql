d50a7f82893d173540a72ad8c7fd2c475aea1213
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-10-23 15:57:03 -0700

    drop tables before create, load some reviews

--- sql/create.sql
+++ sql/create.sql
@@ -1,3 +1,5 @@
+DROP TABLE Movie, Actor, MovieGenre, MovieActor, Review;
+
 CREATE TABLE Movie(id int PRIMARY KEY, title varchar(100), year int, rating varchar(10), company varchar(50));
 CREATE TABLE Actor(id int PRIMARY KEY, last varchar(20), first varchar(20), sex varchar(6), dob date, dod date);
 CREATE TABLE MovieGenre(mid int, genre varchar(20));
