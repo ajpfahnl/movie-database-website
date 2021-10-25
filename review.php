3c4ebc589c11a295ae4879ff7c0ee4665f8eae3e
vanessawang12 <vanillawang12@gmail.com>
2021-10-24 18:09:18 -0700

    finished review.php

--- review.php
+++ review.php
@@ -27,106 +27,37 @@ if ($db->connect_errno > 0) {
 
 echo "<h1>Add New Review Here:</h1>";
 
-// $movie_id = $_GET["id"];
-// $movie_title = $_GET["title"];
-
 echo "<h2>Movie Title: </h2>";
 
-// search movie title here
-
-$movie = "SELECT DISTINCT * FROM Movie WHERE title<>'NULL' AND id={$_GET["movie"]}";
-//echo "test query<br>";
+$movie = "SELECT DISTINCT * FROM Movie WHERE title<>'NULL' AND id={$_GET["id"]}";
 $rs = $db->query($movie);
-//echo "after query<br>";
 
 while ($row = $rs->fetch_assoc()) {
-  //echo "after fetch<br>";
   $mid = $row['id'];
   $title = $row['title'];
 
-  // echo "$title";
   echo "<tr>
           <td><b>$title</b></td><br><br>
         </tr>";
 }
-
-/*
-// echo "<form>
-echo "<form action=\"./review.php\" method=\"get\">
-    Your Name: <input type=\"text\" name=\"username\"><br>
-    Rating (From 1 to 10): <input type=\"text\" name=\"rating\"><br>
-    Comment: <textarea></textarea><br><br>
-    <input type=\"submit\" value=\"Rating It!\">
-</form>";
-*/
 
 function SaveToMyOwnDatabase() {
   $db = new mysqli('localhost', 'cs143', '', 'class_db');
   if ($db->connect_errno > 0) { 
       die('Unable to connect to database [' . $db->connect_error . ']'); 
   }
-  // $add_review =  "INSERT INTO Review VALUES ({$_GET["name"]}, now(), {$_GET["movie"]}, {$_GET["rating"]}, {$_GET["comment"]})";
-  $add_review =  "INSERT INTO Review VALUES (\"Van\", now(), {$_GET["movie"]}, {$_GET["rating"]}, {$_GET["comment"]})";
-  // $add_review =  "INSERT INTO Review VALUES (\"Van\", now(), 705, 10, \"good\")";
-  // $add_review =  "INSERT INTO Review VALUES (\"Van\", now(), 705, 10, \"good\")";
+  $add_review =  "INSERT INTO Review VALUES (\"{$_GET["name"]}\", now(), {$_GET["id"]}, {$_GET["rating"]}, \"{$_GET["comment"]}\")";
   $rs2 = $db->query($add_review);
   
-  /*
-  echo "name: {$_GET["name"]}";
-  echo "movie: {$_GET["movie"]}";
-  echo "rating: {$_GET["rating"]}";
-  echo "comment: {$_GET["comment"]}";
-  */
-
-  // echo "console.log(\"name: {$_GET["name"]}\");"
-
-  /*
-  console.log("name: {$_GET["name"]}");
-  console.log("movie: {$_GET["movie"]}");
-  console.log("rating: {$_GET["rating"]}");
-  console.log("comment: {$_GET["comment"]}");
-  */
 }
 
-// action=\"./review.php\" method=\"get\" <form id=\"myForm\" onSubmit=\"SaveToMyOwnDatabase()\">
 
-// echo "<form action=\"./review.php\" method=\"get\" <form id=\"myForm\" onSubmit=\"SaveToMyOwnDatabase()\">
-//     <input type=\"hidden\" id=\"movie\" name=\"movie\" value={$_GET["movie"]}>
-//     <label for=\"name\">Your name:</label>
-//     <input type=\"text\" id=\"name\" name=\"name\"><br>
-    
-//     <label for=\"rating\">Rating (Out of 10):</label>
-//     <input type=\"text\" id=\"rating\" name=\"rating\"><br>
-    
-//     <label for=\"comment\">Comment:</label>
-//     <textarea id=\"comment\" name=\"comment\" rows=\"4\" cols=\"50\"></textarea><br><br>
-    
-//     <input type=\"submit\" onSubmit=\"SaveToMyOwnDatabase()\" value=\"Rating It!\">
+?>
 
-//     </form>";
+<form id="myForm" onSubmit=<?php SaveToMyOwnDatabase() ?>>
+    <input type="hidden" id="id" name="id" value=<?php echo "{$_GET["id"]}" ?>>
+    <input type="hidden" id="submitted" name="submitted" value="TRUE" ?>>
 
-// Adding review to the Review Table
-// $add_review =  "INSERT INTO Review (name, now(), mid, rating, comment)";
-// $rs2 = $db->query($add_review);
-
-// After submission, print out confirmation text
-// echo "Thanks for your comment! Your review has been successfully added. ";
-// echo "DONE1<br>";
-// $add_review =  "INSERT INTO Review VALUES (\"Van\", now(), 705, 10, \"good\")";
-// echo "DONE2<br>";
-// $rs2 = $db->query($add_review);
-// echo "DONE3<br>";
-//SaveToMyOwnDatabase();
-// echo "DONE4<br>";
-// redirect review page to movie page
-
-// if(array_key_exists('submitbutton', $_GET)){
-//   SaveToMyOwnDatabase();
-// }
-
-print '<form id="myForm" onSubmit=<?php SaveToMyOwnDatabase() ?>>
-    <input type="hidden" id="movie" name="movie" value=<?php echo "{$_GET["movie"]}" ?>>
-    
     <label for="name">Your name:</label>
     <input type="text" id="name" name="name" value=<?php echo "{$_GET["name"]}" ?>><br>
     
@@ -138,7 +69,17 @@ print '<form id="myForm" onSubmit=<?php SaveToMyOwnDatabase() ?>>
     
     <input type="submit" value="Rating It!">
 
-</form>';
+</form>
+
+<?php
+
+  $mid = $_GET["submitted"];
+  if( $mid === "TRUE"){
+      echo "Thanks for your comment! Your review has been successfully added. ";
+      echo "<a href=\"/movie.php?id={$_GET["id"]}\">
+             click this to go back to see the movie
+            </a>";
+  }
 
 ?>
 
